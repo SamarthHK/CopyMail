152f911effc9ce377b58cc5ee22162827ebdfb91
Viveka-01 <SamarthHKPersonal@outlook.com>
2026-02-12 23:48:11 -0800

    Removed my current config file, Now have a working program I am satisfied with.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import utils 
 import keyboard
-import smtplib
 import pyperclip
-temp = True
-while True:
-    if keyboard.is_pressed(utils.configReader("key")) and temp:
-        utils.email(utils.configReader("send"),utils.configReader("receive"),pyperclip.paste(),utils.configReader("password"))
-        temp = False
-    elif not  keyboard.is_pressed(utils.configReader("key")):
-        temp = True
+import time
+if __name__ == "__main__":
+    if utils.configCheck() == "missing":
+        print("You dont have any configurations setup.\n Setting up configs...")
+        utils.configCreate(*utils.configInput())
+    temp = True
+    while True:
+        if keyboard.is_pressed(utils.configReader("key")) and temp:
+            utils.email(utils.configReader("send"),utils.configReader("receive"),pyperclip.paste(),utils.configReader("password"))
+            temp = False
+        elif not  keyboard.is_pressed(utils.configReader("key")):
+            temp = True
+    time.sleep(0.1)
+
+
         
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import json
 import smtplib
 from email.message import EmailMessage
+from pathlib import Path
+import keyboard
+import re
 
 def configReader(key:str):
     with open("config.json","r") as config:
@@ -27,3 +30,49 @@ def email(sender:str,receiver:str,copyMail:str,password:str):
         server.starttls()  
         server.login(sender, password)
         server.send_message(msg)
+def configCheck():
+    configPath = Path("config.json")
+    if configPath.exists():
+        return "found"
+    else:
+        return "missing"
+def configCreate(sender:str,receiver:str,password:str,keybind:str):
+    config = {
+        "email": {
+            "sender": sender,
+            "receiver": receiver,
+            "app_password": password
+        },
+        "hotkey": keybind
+    }
+    with open("config.json","w") as configFile:
+        json.dump(config, configFile, indent=4)
+def configInput():
+    sender = senderEmailCheck()
+    password = input("Provide the app password for the email you just inputted.\n")
+    receiver = recieverEmailCheck()
+    print("Press the keys you wish to use for your keybinds.\n Press escape to stop.")
+    keybind = keybindReader()
+    return sender,receiver,password,keybind
+def keybindReader():
+    events = keyboard.record(until='escape', suppress=False)
+    pressedKeys = set()
+    for event in events:
+        if event.event_type == "down" and event.name != "escape":
+            pressedKeys.add(event.name)
+    pressedKeys.remove("esc")
+    keybind = "+".join(sorted(pressedKeys))
+    return keybind
+def emailCheck(email):
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    return re.match(pattern, email) is not None
+def senderEmailCheck():
+    sender = input("Input the email you will use to send an email.\n")
+    while not emailCheck(sender):
+        sender = input("Input a valid email you will use to send an email.\n")
+    return sender 
+def recieverEmailCheck():
+    receiver = input("Input the email you will use to receive the email.\n")
+    while not emailCheck(receiver):
+        receiver = input("Input a valid email you will use to receive the email.\n")
+    return receiver 
