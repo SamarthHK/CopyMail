209b9caa53f18c4f7a441206ed5cd2f4487ebe13
Viveka-01 <SamarthHKPersonal@outlook.com>
2026-02-12 18:11:07 -0800

    Works with forever loop, can send email on press of keybind

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import utils
 import keyboard
 import smtplib
 import pyperclip
-utils.email(utils.configReader("send"),utils.configReader("receive"),pyperclip.paste(),utils.configReader("password"))
+temp = True
+while True:
+    if keyboard.is_pressed(utils.configReader("key")) and temp:
+        utils.email(utils.configReader("send"),utils.configReader("receive"),pyperclip.paste(),utils.configReader("password"))
+        temp = False
+    elif not  keyboard.is_pressed(utils.configReader("key")):
+        temp = True
+        
+
 
 
 
